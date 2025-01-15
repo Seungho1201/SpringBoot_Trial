600ade15158eab78771b474b72716702f6991c54
Seungho1201 <ohseungho1201@gmail.com>
2025-01-16 00:11:52 +0900

    Controller

---
+++ shop/src/main/java/com/seungho/shop/BasicController.java
@@ -0,0 +1,24 @@
+package com.seungho.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/")
+    //@ResponseBody 는 문자 그대로 보내주세요란 의미 빼야 html 파일이 return됨
+    
+    String hello(){
+        return "index.html";
+    }
+
+
+    @GetMapping("/about")
+    @ResponseBody
+
+    String hello2(){
+        return "Hello World2";
+    }
+}
