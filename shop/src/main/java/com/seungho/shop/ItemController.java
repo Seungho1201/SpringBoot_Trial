d23ff05e43d5ba852d74020e07d9166f395c7ef1
Seungho1201 <ohseungho1201@gmail.com>
2025-01-17 22:25:26 +0900

    detail page (Optional)

--- shop/src/main/java/com/seungho/shop/ItemController.java
+++ shop/src/main/java/com/seungho/shop/ItemController.java
@@ -3,12 +3,10 @@ package com.seungho.shop;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,19 +25,30 @@ public class ItemController {
         return "list.html";
     }
 
-
     @GetMapping("/write")
     String write() {
         return "write.html";
     }
 
-
     @PostMapping("/add")
     String addPost(@ModelAttribute Item item) { // Item 클래스의 오브젝트를 하나 뽑아서 바로 넣어줌
-
         itemRepository.save(item);
 
         return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model) {
+
+        Optional<Item> result = itemRepository.findById(id);
+
+        if(result.isPresent()) {
+            model.addAttribute("item", result.get());
+        } else {
+            return "redirect:/list";
+        }
+
+        return "detail.html";
     }
 
 }
--- shop/src/main/java/com/seungho/shop/ItemRepository.java
+++ shop/src/main/java/com/seungho/shop/ItemRepository.java
@@ -2,6 +2,7 @@ package com.seungho.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// JpaRepository<Item, Long>  테이블 이름과 기본키가 되는 테이블의 데이터 타입 가져온거임
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
